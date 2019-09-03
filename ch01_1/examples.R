10f855e030ee4a3da5691f3c77993419eb505d26
Mike Harris <MikeMKH@users.noreply.github.com>
2019-09-03 06:09:01 -0500

    add examples from 1.1

---
+++ ch01_1/examples.R
@@ -0,0 +1,12 @@
+hello <- "Hello"
+world <- "World"
+paste(hello, world)
+
+greet <- function (name) { print(paste(hello, name)) }
+greet("Mike")
+
+name <- readline()
+greet(name)
+
+birthday <- function (name) { print(paste0("Happy birthday ",name,"!")) }
+birthday("Jack")
