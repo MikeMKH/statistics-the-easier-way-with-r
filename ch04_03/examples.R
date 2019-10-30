6e157941a124004a88308dc6f81a6c0f2dfbef89
Mike Harris <MikeMKH@users.noreply.github.com>
2019-10-30 06:26:49 -0500

    add examples from chapter 4.3

---
+++ ch04_03/examples.R
@@ -0,0 +1,5 @@
+source("https://raw.githubusercontent.com/NicoleRadziwill/R-Functions/master/cipaired.R")
+
+#pg 263
+ci.paired(n=12,dbar=8.6,s.d=6.5,conf.level=0.95)
+ci.paired(n=12,dbar=8.6,s.d=6.5,conf.level=0.999999)
