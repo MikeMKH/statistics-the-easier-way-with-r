8a209a5165da1646e1afed6e80463cdc210a3dbd
Mike Harris <MikeMKH@users.noreply.github.com>
2019-11-27 06:34:48 -0600

    add dewpoint and temp linear model example

---
+++ ch06_01/examples.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(janitor)
+
+#pg 431
+(shd <- janitor::clean_names(
+  read_delim("https://raw.githubusercontent.com/NicoleRadziwill/Data-for-R-Examples/master/kshd-2013.txt", delim="\t1")))
+
+#pg 432
+(slope <- cor(shd$dewp, shd$temp) * (sd(shd$dewp)/sd(shd$temp)))
+(intercept <- mean(shd$dewp) - (slope*mean(shd$temp)))
+
+fit <- lm(dewp~temp,data=shd)
+shd$residuals <- resid(fit)
+summary(fit)
+
+#pg 433
+left <- shd %>% 
+  ggplot(aes(x=temp, y=dewp)) +
+  geom_point() +
+  geom_smooth(aes(color="red"), se=FALSE, method="lm") +
+  ggtitle("Dewpoint vs. Temperature") +
+  theme(legend.position="none")
+right <- shd %>%
+  ggplot(aes(x=temp, y=residuals)) +
+  geom_point() +
+  geom_hline(yintercept=0) +
+  ggtitle("Residuals")
+cowplot::plot_grid(left,right)
+
+#pg 436
+cor(shd$dewp,shd$temp)
+cor(shd$dewp,shd$temp)^2
+
+summary(fit)$r.squared
+
+#pg 437
+par(mfrow=c(2,2))
+plot(fit)
